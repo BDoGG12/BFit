da6f44814463f2a85c51fe3ad26fd07f6f21c76e
Ben Do <bdo.appworkshop@gmail.com>
2025-11-09 14:20:04 -0600

    Initial Commit

---
+++ BFit/BFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BFitApp.swift
+//  BFit
+//
+//  Created by Ben Do on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct BFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BFitTests/BFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  BFitTests.swift
+//  BFitTests
+//
+//  Created by Ben Do on 11/9/25.
+//
+
+import Testing
+@testable import BFit
+
+struct BFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
