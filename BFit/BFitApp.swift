bf706d29a23346055b107011ccb88c0dc5d7a2a9
Ben Do <bdo.appworkshop@gmail.com>
2025-11-16 10:47:08 -0600

    Configured FirebaseApp on appear.

--- BFit/BFitApp.swift
+++ BFit/BFitApp.swift
@@ -7,12 +7,16 @@
 
 import SwiftUI
 import GoogleSignIn
+import FirebaseCore
 
 @main
 struct BFitApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear(perform: {
+                    FirebaseApp.configure()
+                })
                 .onOpenURL { url in
                     GIDSignIn.sharedInstance.handle(url)
                 }
