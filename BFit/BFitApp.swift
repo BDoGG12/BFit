869bd41e515258ca6920aba20fa6ed0814f805fe
Ben Do <bdo.appworkshop@gmail.com>
2025-11-17 12:09:35 -0600

    Inserted AppDelegate class to configure Firebase app. Removed onAppear method to configure Firebase.

--- BFit/BFitApp.swift
+++ BFit/BFitApp.swift
@@ -9,14 +9,25 @@ import SwiftUI
 import GoogleSignIn
 import FirebaseCore
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
 @main
 struct BFitApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onAppear(perform: {
-                    FirebaseApp.configure()
-                })
                 .onOpenURL { url in
                     GIDSignIn.sharedInstance.handle(url)
                 }
