33021b2de156b87df26299f42d4e9c5a7408c6c3
Ben Do <bdo.appworkshop@gmail.com>
2025-11-20 16:10:29 -0600

    Edited Text fonts and inserted apple button skeleton to sign in with Apple

--- BFit/View/AuthViews/LoginView.swift
+++ BFit/View/AuthViews/LoginView.swift
@@ -12,14 +12,26 @@ import GoogleSignIn
 struct LoginView: View {
     var body: some View {
         VStack {
-            Text("Login")
-                .font(.headline)
+            Text("Login with:")
+                .font(.largeTitle)
                 .padding()
             GoogleSignInButton {
                 handleSignInButton()
             }
             .frame(width: 10.0, height: 50.0, alignment: .center)
             .buttonStyle(.borderedProminent)
+            Text("or")
+                .font(.title)
+                .fontWeight(.medium)
+                .padding()
+            Button("Apple") {
+                print("Sign in with Apple")
+            }
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .background(.black)
+            .cornerRadius(10)
         }
     }
     func handleSignInButton() {
