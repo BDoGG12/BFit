9f7e93d21f46533020c065208d69cd6e1fc23cce
Ben Do <bdo.appworkshop@gmail.com>
2025-11-23 14:08:55 -0600

    Added sign up buttons for SignUpView with Google and Apple.

--- BFit/View/AuthViews/SignUpView.swift
+++ BFit/View/AuthViews/SignUpView.swift
@@ -6,10 +6,42 @@
 //
 
 import SwiftUI
+import GoogleSignInSwift
+import GoogleSignIn
+import AuthenticationServices
 
 struct SignUpView: View {
     var body: some View {
-        Text("Sign Up View")
+        VStack {
+            Text("Sign up with:")
+                .font(.title)
+                .bold()
+            
+            GoogleSignInButton {
+                Task {
+                    
+                }
+            }
+            .frame(width: 200, height: 44, alignment: .center)
+            
+            Text("or")
+                .font(.title)
+                .fontWeight(.medium)
+                .padding()
+            
+            // Apple Sign In
+            SignInWithAppleButton { idRequest in
+                print(idRequest.user ?? "Unknown User")
+            } onCompletion: { result
+                in
+                print("Result: \(result)")
+            }
+            .frame(width: 200, height: 44, alignment: .center)
+        }
+    }
+    
+    func handleSignInButton() {
+        
     }
 }
 
