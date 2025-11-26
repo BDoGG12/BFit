27e71f7ea854b8a3d272bbe68c98aad9215bc4c1
Ben Do <bdo.appworkshop@gmail.com>
2025-11-26 16:40:42 -0600

    Removed TabView.

--- BFit/ContentView.swift
+++ BFit/ContentView.swift
@@ -9,17 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            Tab("Home", systemImage: "house.fill") {
-                HomeView()
-            }
-            Tab("Calculator", systemImage: "plus.forwardslash.minus") {
-                UserInputView()
-            }
-            Tab("Profile", systemImage: "person.fill") {
-                ProfileView()
-            }
-        }
+        UserInputView()
         
     }
 }
