47795a365b4fde81249272a92caa2393d6c5424d
Ben Do <bdo.appworkshop@gmail.com>
2025-12-17 15:34:18 -0600

    Implemented methods getNextDate and convertToDate.

--- BFit/DateManager.swift
+++ BFit/DateManager.swift
@@ -8,13 +8,22 @@
 import Foundation
 
 class DateManager: ObservableObject {
-    
+    let dateFormatter = DateFormatter()
+
     
     func getCurrentDate() -> String {
-        let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         return dateFormatter.string(from: Date())
     }
     
+    func getNextDate() -> String {
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let nextDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+        return dateFormatter.string(from: nextDate)
+    }
     
+    func convertToDate(_ dateString: String) -> Date? {
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.date(from: dateString)
+    }
 }
