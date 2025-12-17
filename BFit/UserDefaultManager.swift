c9f9c5867c40a21ca3ef814ec420dd4b461b87aa
Ben Do <bdo.appworkshop@gmail.com>
2025-12-17 14:32:40 -0600

    Added UserDefaultManager class.

---
+++ BFit/UserDefaultManager.swift
@@ -0,0 +1,19 @@
+//
+//  UserDefaultManager.swift
+//  BFit Calculator
+//
+//  Created by Ben Do on 12/17/25.
+//
+
+import Foundation
+
+class UserDefaultManager: UserDefaults {
+    
+    func store(value: Any?, forKey key: String) {
+        set(value, forKey: key)
+    }
+    
+    func saveUsageAmount(amount: Int) {
+        UserDefaults.standard.set(amount, forKey: "usageAmt")
+    }
+}
