4804146fae9f478c89b571c303c53763f15db777
Ben Do <bdo.appworkshop@gmail.com>
2026-02-16 10:15:57 -0600

    Uncommented out Purchases configuration.

--- BFit/BFitApp.swift
+++ BFit/BFitApp.swift
@@ -21,14 +21,14 @@ struct BFitApp: App {
         showOnboarding = !appHasLaunched
         print("Shows onboarding: \(showOnboarding)")
         // Verbose logs while integrating
-//        Purchases.logLevel = .debug
-//        
-//        // Configure Purchases once at app launch
-//        Purchases.configure(with: Configuration
-//            .builder(withAPIKey: AppSecrets.revenueCatAPIKey)
-//            .with(storeKitVersion: .storeKit2)
-//            .build()
-//        )
+        Purchases.logLevel = .debug
+        
+        // Configure Purchases once at app launch
+        Purchases.configure(with: Configuration
+            .builder(withAPIKey: AppSecrets.revenueCatAPIKey)
+            .with(storeKitVersion: .storeKit2)
+            .build()
+        )
     }
     
     var body: some Scene {
