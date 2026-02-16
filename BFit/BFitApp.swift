f5ba0248c5c1dbb5e5fac8a83afffd5991a2d718
Ben Do <bdo.appworkshop@gmail.com>
2026-02-16 09:46:46 -0600

    Commented out RevenueCat functionalities.

--- BFit/BFitApp.swift
+++ BFit/BFitApp.swift
@@ -21,14 +21,14 @@ struct BFitApp: App {
         showOnboarding = !appHasLaunched
         print("Shows onboarding: \(showOnboarding)")
         // Verbose logs while integrating
-        Purchases.logLevel = .debug
-        
-        // Configure Purchases once at app launch
-        Purchases.configure(with: Configuration
-            .builder(withAPIKey: AppSecrets.revenueCatAPIKey)
-            .with(storeKitVersion: .storeKit2)
-            .build()
-        )
+//        Purchases.logLevel = .debug
+//        
+//        // Configure Purchases once at app launch
+//        Purchases.configure(with: Configuration
+//            .builder(withAPIKey: AppSecrets.revenueCatAPIKey)
+//            .with(storeKitVersion: .storeKit2)
+//            .build()
+//        )
     }
     
     var body: some Scene {
@@ -39,9 +39,9 @@ struct BFitApp: App {
                 })
                 .environmentObject(revenueCat)
                 // Listen for customer info updates for the entire app lifecycle
-                .task {
-                    await revenueCat.startCustomerInfoListener()
-                }
+//                .task {
+//                    await revenueCat.startCustomerInfoListener()
+//                }
         }
     }
 }
