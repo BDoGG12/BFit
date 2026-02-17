34d60abf3a2f3f47747c0e51f13db381c0e17db2
Ben Do <bdo.appworkshop@gmail.com>
2026-02-17 14:34:57 -0600

    Added BMIResultsView.

---
+++ BFit/View/ResultsView/BMIResultsView.swift
@@ -0,0 +1,43 @@
+//
+//  BMIResultsView.swift
+//  BFit Calculator
+//
+//  Created by Ben Do on 2/17/26.
+//
+
+import SwiftUI
+
+struct BMIResultsView: View {
+    @Environment(\.dismiss) var dismiss
+    let result: Double
+    let bmiZone: String
+    let color: Color
+    var body: some View {
+        VStack {
+            Text("Your Body Fat Percentage is \(Int(result.rounded()))%!")
+                .font(.system(size: 32, weight: .bold, design: .default))
+                .multilineTextAlignment(.center)
+            Text("You are in the ")
+                .font(.system(size: 25, weight: .bold, design: .default))
+                .multilineTextAlignment(.center)
+            Text("\(bmiZone) Zone!")
+                .font(.system(size: 30, weight: .bold, design: .default))
+                .foregroundStyle(color)
+            Button("Ok") {
+                dismiss()
+            }
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(10)
+        }
+        .background {
+            Image("results_fit")
+        }
+    }
+}
+
+#Preview {
+    BMIResultsView()
+}
